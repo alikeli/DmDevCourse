c274561ee53af80c4fa56c95aa13959662175cfd
alikeli <eli-com2@yandex.ru>
2023-03-22 20:22:10 +0100

    else-if

--- src/main/java/org/example/if_else/Lesson4.java
+++ src/main/java/org/example/if_else/Lesson4.java
@@ -2,10 +2,15 @@ package org.example.if_else;
 
 public class Lesson4 {
     public static void main(String[] args) {
-        int value = 10;
+        int value = 0;
+        boolean test = value > 0;
 
-        if (value > 0) {
+        if (test) {
             System.out.println("number is positive");
+        } else if (value == 0) {
+            System.out.println("number = 0");
+        } else {
+            System.out.println("number is negative");
         }
     }
 }
