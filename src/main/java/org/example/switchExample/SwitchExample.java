ae485154b77dea8ceb64a2f74936cdb182022faf
alikeli <eli-com2@yandex.ru>
2023-03-22 20:33:20 +0100

    switch

---
+++ src/main/java/org/example/switchExample/SwitchExample.java
@@ -0,0 +1,32 @@
+package org.example.switchExample;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int value = 2;
+        switch (value) {
+            case 1:
+                System.out.println("first");
+                break;
+            case 2:
+                System.out.println("second");
+                break;
+            case 30:
+                System.out.println("thirtieth");
+                break;
+            default:
+                System.out.println("number not found");
+        }
+
+
+        int value1 = 2;
+        switch (value1) {
+            case 11:
+            case 22:
+            case 33:
+                System.out.println("show numbers");
+                break;
+            default:
+                System.out.println("number not found");
+        }
+    }
+}
