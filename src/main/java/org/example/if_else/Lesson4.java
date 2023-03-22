15148744d60d470427596b548119f804334b5f6d
alikeli <eli-com2@yandex.ru>
2023-03-22 20:16:29 +0100

    Initial commit

---
+++ src/main/java/org/example/if_else/Lesson4.java
@@ -0,0 +1,11 @@
+package org.example.if_else;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        int value = 10;
+
+        if (value > 0) {
+            System.out.println("number is positive");
+        }
+    }
+}
